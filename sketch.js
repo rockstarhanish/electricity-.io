7551d1f326489a43cd01d7a65da777d8347b1c17
rockstarhanish <83698955+rockstarhanish@users.noreply.github.com>
2021-10-01 08:56:06 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,12 @@
+var bg;
+function preload() {
+bg = loadImage("images/920.jpg")
+}
+function setup() {
+  createCanvas(windowWidth,windowHeight);
+}
+
+function draw() {
+  background(bg);  
+  
+}
